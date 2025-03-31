3e5673e8e2cef1b7f750f63cd78bcb812708c5a4
Adhiraj Bhattacharya <adhirajbhattacharya@gmail.com>
2025-04-01 01:48:52 +0530

    added building view worded question

--- company-interviews/src/main/java/com/adhiraj/dsalgo/salesforce/BuildingView.java
+++ company-interviews/src/main/java/com/adhiraj/dsalgo/salesforce/BuildingView.java
@@ -2,15 +2,23 @@ package com.adhiraj.dsalgo.salesforce;
 
 import java.util.*;
 
+/**
+ * Question: You are given a list of integers representing height of buildings
+ * in a row in the order they exist. A building is able to see another building
+ * on its side if all the building in between itself and the other is of lower
+ * height than both. Return a list which where each number is the number of
+ * buildings it can see on both sides.
+ */
 public class BuildingView {
 
     public static void main(String[] args) {
         List<int[]> inputs = new ArrayList<>();
         inputs.add(new int[] {5, 2, 10, 1});
         inputs.add(new int[] {3, 6, 2, 4, 5});
+        inputs.add(new int[] {3, 6, 2, 8, 7});
         inputs.stream()
                 .peek(arr -> {
-                    if (!Arrays.equals(totalViewBruteForce(arr), totalViewMonoStackSinglePass(arr)))
+                    if (!Arrays.equals(totalViewBruteForce(arr), totalViewMonoStack(arr)))
                         throw new RuntimeException("Brute force and optimized solutions do not match for " + Arrays.toString(arr));
                 })
                 .map(BuildingView::totalViewMonoStack).map(Arrays::toString).forEach(System.out::println);
@@ -24,6 +32,7 @@ public class BuildingView {
         Deque<Integer> leftStack = new ArrayDeque<>();
         Deque<Integer> rightStack = new ArrayDeque<>();
 
+        // {3, 6, 2, 8, 7}
         // Traverse the buildings from left to right and right to left in the same loop
         for (int i = 0; i < n; i++) {
             // Left visibility (moving left to right)
@@ -51,9 +60,9 @@ public class BuildingView {
         int n = height.length;
         int[] view = new int[n];
 
-        // Calculate right visibility
         Deque<Integer> stack = new ArrayDeque<>();
 
+        // {3, 6, 2, 8, 7}
         // Calculate left visibility
         for (int i = 0; i < n; i++) {
             while (!stack.isEmpty() && height[stack.peek()] < height[i]) {
@@ -66,6 +75,7 @@ public class BuildingView {
 
         stack.clear();
 
+        // Calculate right visibility
         for (int i = n - 1; i >= 0; i--) {
             while (!stack.isEmpty() && height[stack.peek()] < height[i]) {
                 view[i]++; // Count the visible building
